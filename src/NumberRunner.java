e8a9f88ff90136703ac9c48b28cb0dfec2e5540c
test <annac101@nycstudents.net>
2022-10-17 23:40:31 -0400

    final?

---
+++ src/NumberRunner.java
@@ -0,0 +1,7 @@
+public class NumberRunner {
+    public static void main(String[] args){
+        LuckyNumbers number = new LuckyNumbers();
+        String luckynum = number.getLuckyNumbers();
+        System.out.print(luckynum);
+    }
+}
---
+++ src/math.java
@@ -0,0 +1,15 @@
+public class math {
+    public static void main (String[] args){
+                System.out.println(Math.abs(-1));
+                System.out.println(Math.abs(-1.5));
+                System.out.println(Math.abs(1));
+                System.out.println(Math.abs(1.5));
+
+                System.out.println(Math.round(-1.25));
+                System.out.println(Math.round(-1.5));
+                System.out.println(Math.round(-1.75));
+                System.out.println(Math.round(1.25));
+                System.out.println(Math.round(1.5));
+                System.out.println(Math.round(1.75));
+    }
+}
